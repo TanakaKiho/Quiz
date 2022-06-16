c78a79e633acd7fb1cec203a5e0a91c698440763
Tanaka <moon.kiho.premier@gmail.com>
2022-06-16 15:49:58 +0900

    本日作業分

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.quiz
 
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setContentView(R.layout.top)
-
+/*
         // adapter作成
         val array = arrayOf("A", "B", "C", "D")
         val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, array)
@@ -42,6 +40,18 @@ class MainActivity : AppCompatActivity() {
         // 選択をリセット
         buttonReset.setOnClickListener {
             spinner.setSelection(0)
+        }*/
+        val spinner: Spinner = findViewById(R.id.spinner)
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+                this,
+                R.array.planets_array,
+                android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
         }
     }
 }
